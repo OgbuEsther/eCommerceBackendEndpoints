3a8b59cb28f0bb93f3f9c2f6a2dca391edb84598
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-02-21 10:43:40 +0100

    saved controller

--- controller/PracticeProductController.ts
+++ controller/PracticeProductController.ts
@@ -5,4 +5,29 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 const router = express.Router();
 
-//
+// create product
+
+router.post("/api/product", async (req: Request, res: Response) => {
+  try {
+    const { title, description, price, quantity, category, status } = req.body;
+
+    const newProduct = await productModels.create({
+      title,
+      description,
+      price,
+      quantity,
+      category,
+      status,
+    });
+
+    return res.status(201).json({
+      message: "Product successfully created",
+      data: newProduct,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      message: "unable to create product",
+      data: error,
+    });
+  }
+});
