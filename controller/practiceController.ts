fd2d56c74ebef69dde6f04041af8e6ab2f80632d
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-02-21 08:31:45 +0100

    practice files

---
+++ controller/practiceController.ts
@@ -0,0 +1,5 @@
+import UserModel from "../models/UserModel";
+import productModels from "../models/productModels";
+import express, { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
