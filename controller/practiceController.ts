b23790301822b91868e75a3b6ec5f5dca5d9e2d3
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-02-21 08:37:17 +0100

    saved controller

--- controller/practiceController.ts
+++ controller/practiceController.ts
@@ -3,3 +3,21 @@ import productModels from "../models/productModels";
 import express, { Request, Response } from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+
+const router = express.Router();
+
+//get all users
+router.get("/users", async (req: Request, res: Response) => {
+  try {
+    const fetchUsers = await UserModel.find();
+
+    return res.status(200).json({
+      message: "Success",
+      data: fetchUsers,
+    });
+  } catch (error) {
+    return res.status(404).json({
+      message: "User not found",
+    });
+  }
+});
