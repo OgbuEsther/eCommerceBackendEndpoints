91e8c166a4ffb5f41690bb203d108550926e0a80
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-02-21 08:07:17 +0100

    practice files

--- src/practiceIndex.ts
+++ src/practiceIndex.ts
@@ -1,5 +1,22 @@
-import express from "express"
+import express from "express";
 
-import from cors from "cors"
+import cors from "cors";
 
-import mongoose from 
+import mongoose from "mongoose";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port: number | string = process.env.PORT || 2004;
+
+const DB_URL = "mongodb://0.0.0.0:27017/practiceEcommerce";
+
+mongoose
+  .connect(DB_URL)
+  .then((conn) => {
+    console.log(`database connection opened`);
+  })
+  .catch((err) => {
+    console.log(`error`, err);
+  });
