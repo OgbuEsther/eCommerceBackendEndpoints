a5fac443444ae3100acacff7f5e0e0e6c766ad2e
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-02-23 18:54:53 +0100

    saved changes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-// import AllRoute from "./Controller/AllController";
+import AllRoute from "../controller/allController";
 const port = 4573;
 const url = "mongodb://0.0.0.0:27017/set06reduxEcommerce";
 const app = express();
@@ -12,7 +12,7 @@ mongoose.connect(url).then(() => {
   console.log("database connection established");
 });
 
-// app.use("/api", AllRoute);
+app.use("/api", AllRoute);
 
 app.listen(port, () => {
   console.log("listening on port");
